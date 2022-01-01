d53638bc5bfa0404e92aa526bfcd1ed266696bf2
dazmaks <mysagaz@mail.ru>
2022-01-02 01:31:50 +0300

    Add comments

--- src/bin/person-detector.rs
+++ src/bin/person-detector.rs
@@ -3,42 +3,40 @@ extern crate image;
 
 use person_detector::skin::rgb_check;
 
-use std::env;
-use std::process::exit;
+use std::{env, process};
 
-use image::Rgb;
-use image::GenericImageView;
+use image::{Rgb, GenericImageView};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         println!("Usage: {} <input> <output>", args[0]);
-        exit(1);
+        process::exit(1);
     }
 
     let img = image::open(&args[1]).unwrap();
 
-    let black: Rgb<u8> = image::Rgb([0, 0, 0]);
-    let white: Rgb<u8> = image::Rgb([255, 255, 255]);
+    const BLACK: Rgb<u8> = Rgb([0, 0, 0]);
+    const WHITE: Rgb<u8> = Rgb([255, 255, 255]);
 
-	let width = img.width();
-	let height = img.height();
+    // Opening image and converting in to rgb8
+	let mut mut_img = image::open(&args[1]).unwrap().into_rgb8();
 
-	let mut mut_img = img.clone().into_rgb8();
+    // Getting width and height
+	let (width, height) = mut_img.dimensions();
 
 	for x in 0..width {
 		for y in 0..height {
             let pixel = img.get_pixel(x, y);
 
             let color = if rgb_check((pixel[0], pixel[1], pixel[2])) {
-                white
+                WHITE
             } else {
-                black
+                BLACK
             };
 
 			mut_img.put_pixel(x, y, color);
 		}
 	}
-
     mut_img.save(&args[2]).unwrap();
 }
