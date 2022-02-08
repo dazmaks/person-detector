ef41db22a2ab6509a7069277e3ba43fdb6e7405b
dazmaks <mysagaz@mail.ru>
2022-02-08 15:43:26 +0300

    Add fast-math crate

--- src/tsl.rs
+++ src/tsl.rs
@@ -1,14 +1,16 @@
-use std::f64::consts::PI;
+use std::f32::consts::PI;
+use fast_math::atan;
 
-const PI2: f64 = 2. * PI;
+const PI2: f32 = 2. * PI;
+const ONETHIRD: f32 = 1. / 3.;
 
-pub fn ts_from_rgb(r: u8, g: u8, b: u8) -> (f64, f64) {
-	let (rf, gf, bf) = (r as f64, g as f64, b as f64);
+pub fn ts_from_rgb(r: u8, g: u8, b: u8) -> (f32, f32) {
+	let (rf, gf, bf) = (r as f32, g as f32, b as f32);
 	let rgbsum = rf + gf + bf;
 
-	let r1 = rf / rgbsum - 1. / 3.;
-	let g1 = gf / rgbsum - 1. / 3.;
-	let tcalc = (r1 / g1).atan() / PI2;
+	let r1 = rf / rgbsum - ONETHIRD;
+	let g1 = gf / rgbsum - ONETHIRD;
+	let tcalc = atan(r1 / g1) / PI2;
 	let t = if g1 > 0. {
 		tcalc + 1. / 4.
 	} else if g1 < 0. {
